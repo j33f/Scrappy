da18b833b0a88b79c0498e501d649a7a6336b0a2
Jeff <jeff@modulaweb.fr>
2014-08-05 19:01:51 +0200

    determines encoding by reading the page, extract its encoding, then reload the page and convert the buffer

--- routes/examine.js
+++ routes/examine.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var scrap = require('scrap');
 var slugify = require('slug');
+var iconv = require('iconv-lite');
 var fs = require('fs');
 
 var tmpDir = __dirname+'/../public/tmp';
@@ -29,28 +30,46 @@ var collectGarbage = function() {
 router.post('/', function(req, res) {
 	var url = req.param('url');
 	var tmp =  slugify(url)+'.html';
+
+	var storeAndSend = function(html) {
+		try {
+			// try to create the tmp dir in public
+			// ugly thing : fix me
+			fs.mkdirSync(tmpDir);
+		} catch (e) {/* do nothing */}
+
+		fs.unlink(tmpDir+'/'+tmp, function(){
+			// unlink to refresh the ctime
+			fs.writeFile(tmpDir+'/'+tmp, html, function() {
+				// send the data when the file has been written and available fot http queries
+			  res.render('examine', {
+			  	host: req.protocol + '://' + req.get('host')
+			  	, url: url
+			  	, history: '[]'
+			  	, tmp: tmp
+			  });
+			});
+		});
+	}
 	scrap(url, function(err, $, code, html, resp) {
 		if (err) {
 			res.send(500,err);
 		} else {
-			try {
-				// try to create the tmp dir in public
-				// ugly thing : fix me
-				fs.mkdirSync(tmpDir);
-			} catch (e) {/* do nothing */}
-
-			fs.unlink(tmpDir+'/'+tmp, function(){
-				// unlink to refresh the ctime
-				fs.writeFile(tmpDir+'/'+tmp, html, function() {
-					// send the date when the file has been written and available fot http queries
-				  res.render('examine', {
-				  	host: req.protocol + '://' + req.get('host')
-				  	, url: url
-				  	, history: '[]'
-				  	, tmp: tmp
-				  });
+			// try to find the page encoding
+			if($('head meta[http-equiv=\'Content-Type\']').length > 0) {
+				var charset = $('head meta[http-equiv=\'Content-Type\']').attr('content').match(/charset=(.*)/)[1].toLowerCase();
+				scrap({url: url, encoding: null}, function(err, $, code, html, resp) {
+					storeAndSend(iconv.decode(html, charset));
 				});
-			});
+			} else if ($('head meta[charset]').length > 0){
+				var charset = $('head meta[charset]').attr('charset').toLowerCase();
+				scrap({url: url, encoding: null}, function(err, $, code, html, resp) {
+					storeAndSend(iconv.decode(html, charset));
+				});
+			} else {
+				// assume its utf-8
+				storeAndSend(html);
+			}
 		}
 	});
 	collectGarbage();
