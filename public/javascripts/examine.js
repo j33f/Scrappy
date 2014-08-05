06ccb32eca02e0017fb4b65ebfc0ae5ad2975261
Jeff <jeff@modulaweb.fr>
2014-08-05 16:33:39 +0200

    garbage collector for tmp files really fixed + refactor + selector cleanup behavior

--- public/javascripts/examine.js
+++ public/javascripts/examine.js
@@ -1,94 +1,109 @@
+var cleanup = function(selector) {
+	// cleanup a selector by removing parents that have no id attribute but the last one in selector
+	var s = selector.split(' ');
+	var newS = [];
+	s.reverse();
+	for (var i in s) {
+		if (s[i].indexOf('#') == -1) {
+			newS.push(s[i]);
+		} else {
+			newS.push(s[i]);
+			break;
+		}
+	}
+	return newS.reverse().join(' ');
+}
+
 $(function(){
 	(function(old) {
-		// allow to list all attributes by calling $(element).attr()
+	  // allow to list all attributes by calling $(element).attr()
 	  $.fn.attr = function() {
-	    if(arguments.length === 0) {
-	      if(this.length === 0) {
-	        return null;
-	      }
+	  	if(arguments.length === 0) {
+	  		if(this.length === 0) {
+	  			return null;
+	  		}
 
-	      var obj = {};
-	      $.each(this[0].attributes, function() {
-	        if(this.specified) {
-	          obj[this.name] = this.value;
-	        }
-	      });
-	      return obj;
-	    }
+	  		var obj = {};
+	  		$.each(this[0].attributes, function() {
+	  			if(this.specified) {
+	  				obj[this.name] = this.value;
+	  			}
+	  		});
+	  		return obj;
+	  	}
 
-	    return old.apply(this, arguments);
+	  	return old.apply(this, arguments);
 	  };
 	})($.fn.attr);
 
-	var cleanup = function(selector) {
-		var s = selector.split(' ');
-		var newS = [];
-		s.reverse();
-		for (var i in s) {
-			if (s[i].indexOf('#') == -1) {
-				newS.push(s[i]);
-			} else {
-				newS.push(s[i]);
-				break;
-			}
+	$.fn.getSelector = function() {
+		var parents = [];
+		var selector = '';
+		$(this).first().parents()
+			.each(function(index,element) { 
+				var tag = $(element)[0].nodeName.toLowerCase();
+				var id = $(element).attr('id');
+				var classNames = $(element).attr('class');
+				if (id) {
+					parents.push('#'+id);
+				} else if (classNames) {
+					parents.push(tag + '.' + $.trim(classNames).replace(/\s/gi, '.'));
+				} 
+			});
+		if (parents.length > 0) {
+			selector = parents.reverse().join(' ');
 		}
-		return newS.reverse().join(' ');
+		if (selector) { 
+			selector += " "+ $(this)[0].nodeName.toLowerCase();
+		}
+
+		var id = $(this).attr("id");
+		if (id) { 
+			selector += "#"+ id;
+		}
+
+		var classNames = $(this).attr("class");
+		if (classNames) {
+			selector += "." + $.trim(classNames).replace(/\s/gi, ".");
+		}
+		return selector;
 	}
 
 	var pageOffset = $('#page').offset();
+
 	$('#page').height($(window).height() - pageOffset.top - $('#main-form').height());
-	var $page = $('#page').contents();
+
+	var $page = $('#page').contents(); // $page is the iframe content
+
 	$page.ready(function(){
+		// add some styles to the frame
 		$page.find('head').append('<style>body * {cursor: crosshair!important;} .scrappy_selected_element{border: 2px solid red!important}</style>');
+		// when clicking on any element on iframe, unmark all eventually previously marked element, mark it as selected (via a class) and get its selector
 		$page.find("*")
 			.click(function(evt) {
 				evt.preventDefault();
 				$page.find('.scrappy_selected_element').removeClass('scrappy_selected_element');
-				var parents = [];
-				var selector = '';
-				$(this).parents()
-					.each(function(index,element) { 
-						var tag = $(element)[0].nodeName.toLowerCase();
-						var id = $(element).attr('id');
-						var classNames = $(element).attr('class');
-						if (id) {
-							parents.push('#'+id);
-						} else if (classNames) {
-							parents.push(tag + '.' + $.trim(classNames).replace(/\s/gi, '.'));
-						} 
-					});
-				if (parents.length > 0) {
-					selector = parents.reverse().join(' ');
-				}
-				if (selector) { 
-					selector += " "+ $(this)[0].nodeName.toLowerCase();
-				}
-
-				var id = $(this).attr("id");
-				if (id) { 
-					selector += "#"+ id;
-				}
-
-				var classNames = $(this).attr("class");
-				if (classNames) {
-					selector += "." + $.trim(classNames).replace(/\s/gi, ".");
-				}
 				
-				if ($('#cleanup:checked').length) {
+				var selector = $(this).getSelector();
+
+				if ($('#cleanup').is(':checked')) {
+					// cleanup the selector field if the cleanup checkbox is checked
 					$('#current-selector').val(cleanup(selector));
 				} else {
 					$('#current-selector').val(selector);
 				}
 
+				// hilight the selector input field to make the action visible
 				$('#current-selector')
 					.keyup()
 					.effect('highlight', {}, 2000)
 				;
 				return false;
 			});
-	});
+});
 
-	$('#current-selector').keyup(function() {
+$('#current-selector').keyup(function() {
+		// check the current selector string, visually mark them then count them
 		var selector = $(this).val();
 		$page.find('.scrappy_selected_element').removeClass('scrappy_selected_element');
 		try {
@@ -109,135 +124,146 @@ $(function(){
 	});
 });
 
+$('#cleanup').click(function(){
+	var selector = $($('#current-selector').val(), $('#page').contents()).getSelector();
+
+	if ($(this).is(':checked')) {
+		// cleanup the selector field if the cleanup checkbox is checked
+		$('#current-selector').val(cleanup(selector));
+	} else {
+		$('#current-selector').val(selector);
+	}
+});
+
 var actions = {};
 
-	var countTags = function(tag, $el) {
-		var c = 0;
-		for (var i = 0 in $el) {
-			if ($el[i].nodeName) {
-				if ($el[i].nodeName.toLowerCase() == tag) c++;
-			} else {
-				return c;
-			}
+var countTags = function(tag, $el) {
+	var c = 0;
+	for (var i = 0 in $el) {
+		if ($el[i].nodeName) {
+			if ($el[i].nodeName.toLowerCase() == tag) c++;
+		} else {
+			return c;
 		}
-		return c;
+	}
+	return c;
+}
+
+$('#add').click(function(){
+	var selector = $('#current-selector').val();
+	$('#selector-info').html(selector);
+
+	var $elements = $('#page').contents().find(selector);
+	var tags = [];
+
+	$elements.each(function(index, el){
+		var tag = $(el)[0].nodeName.toLowerCase();
+		if ($.inArray(tag, tags) == -1) tags.push(tag);
+	})
+
+	var count = $elements.length;
+	var $action = $('#action');
+	var $attributes = $('#attributes');
+
+	$action.html('<option value="" selected="selected" disabled="disabled">Please choose one</option>');
+	$attributes.html('');
+
+	var specialTagFound = false;
+
+	if ($.inArray('a', tags) >= 0) {
+		specialTagFound = true;
+		var acount = countTags('a', $elements);
+		if (acount == 1) {
+			$action.append('<option value="follow and repeat">Follow the link and repeat other actions</option>');
+			$action.append('<option value="follow and apply new">Follow the link and apply new actions</option>');
+		} else {
+			$action.append('<option value="follow and repeat">Follow the links and repeat other actions (pagination)</option>');
+			$action.append('<option value="follow and apply new">Follow the links and apply new actions</option>');
+		}
+		$action.append('<option value="get href">Get the url</option>');
+		$action.append('<option value="get link text">Get the link text</option>');
+	}
+	if ($.inArray('img', tags) >= 0) {
+		specialTagFound = true;
+		$action.append('<option value="get src">Get the image(s) address</option>');
+		$action.append('<option value="get data url">Get the image(s) data URI</option>');
+	}
+	if ($.inArray('p', tags) >= 0) {
+		specialTagFound = true;
+		var pcount = countTags('p', $elements);
+		if (pcount > 1) {
+			$action.append('<option value="get html">Get each paragraph HTML content separately</option>');
+			$action.append('<option value="get text">Get each paragraph content separately (strip tags)</option>');
+			$action.append('<option value="get html">Get each paragraph HTML content and concat them</option>');
+			$action.append('<option value="get text">Get each paragraph content separately and concat them (strip tags)</option>');
+		} else {
+			$action.append('<option value="get html">Get the paragraph HTML content</option>');
+			$action.append('<option value="get text">Get the paragraph content (strip tags)</option>');				
+		}
 	}
 
-	$('#add').click(function(){
-		var selector = $('#current-selector').val();
-		$('#selector-info').html(selector);
+	if (!specialTagFound) {
+		$action.append('<option value="get html">Get the element HTML content</option>');
+		$action.append('<option value="get text">Get the element content (strip tags)</option>');				
+	}
+	$action.append('<option value="get attributes">None of these, just get attributes</option>');				
 
-		var $elements = $('#page').contents().find(selector);
-		var tags = [];
-
-		$elements.each(function(index, el){
-			var tag = $(el)[0].nodeName.toLowerCase();
-			if ($.inArray(tag, tags) == -1) tags.push(tag);
-		})
-
-		var count = $elements.length;
-		var $action = $('#action');
-		var $attributes = $('#attributes');
-
-		$action.html('<option value="" selected="selected" disabled="disabled">Please choose one</option>');
-		$attributes.html('');
-
-		var specialTagFound = false;
-
-		if ($.inArray('a', tags) >= 0) {
-			specialTagFound = true;
-			var acount = countTags('a', $elements);
-			if (acount == 1) {
-				$action.append('<option value="follow and repeat">Follow the link and repeat other actions</option>');
-				$action.append('<option value="follow and apply new">Follow the link and apply new actions</option>');
-			} else {
-				$action.append('<option value="follow and repeat">Follow the links and repeat other actions (pagination)</option>');
-				$action.append('<option value="follow and apply new">Follow the links and apply new actions</option>');
+	if (tags.length == 1) {
+		var attr = $elements.first().attr();
+		if (Object.keys(attr).length > 0) {
+			for (var k in attr) {
+				$attributes.append('<option value="'+k+'">' + k + '</option>');
 			}
-			$action.append('<option value="get href">Get the url</option>');
-			$action.append('<option value="get link text">Get the link text</option>');
-		}
-		if ($.inArray('img', tags) >= 0) {
-			specialTagFound = true;
-			$action.append('<option value="get src">Get the image(s) address</option>');
-			$action.append('<option value="get data url">Get the image(s) data URI</option>');
-		}
-		if ($.inArray('p', tags) >= 0) {
-			specialTagFound = true;
-			var pcount = countTags('p', $elements);
-			if (pcount > 1) {
-				$action.append('<option value="get html">Get each paragraph HTML content separately</option>');
-				$action.append('<option value="get text">Get each paragraph content separately (strip tags)</option>');
-				$action.append('<option value="get html">Get each paragraph HTML content and concat them</option>');
-				$action.append('<option value="get text">Get each paragraph content separately and concat them (strip tags)</option>');
-			} else {
-				$action.append('<option value="get html">Get the paragraph HTML content</option>');
-				$action.append('<option value="get text">Get the paragraph content (strip tags)</option>');				
-			}
-		}
-
-		if (!specialTagFound) {
-			$action.append('<option value="get html">Get the element HTML content</option>');
-			$action.append('<option value="get text">Get the element content (strip tags)</option>');				
-		}
-		$action.append('<option value="get attributes">None of these, just get attributes</option>');				
-
-		if (tags.length == 1) {
-			var attr = $elements.first().attr();
-			if (Object.keys(attr).length > 0) {
-				for (var k in attr) {
-					$attributes.append('<option value="'+k+'">' + k + '</option>');
-				}
-				$('#attributes-div').show();
-			} else {
-				$('#attributes-div').hide();
-			}
-		}
-
-		$('#add-action-modal').modal();
-	});
-
-	$('#btn-add-action-modal').click(function(){
-		var thisAction = {
-			niceName: $('#nicename').val().trim()
-			, selector: $('#current-selector').val().trim()
-			, action: ($('#action').val() || 'get attributes')
-			, attr: ($('#attributes').val() || [])
-		};
-
-		if (thisAction.niceName == '') {
-				bootbox.alert('Please give a nice name!');
-		} else if (actions[thisAction.niceName]) {
-			bootbox.alert('This name is already in use !');
-		} else if ((thisAction.action == 'get attributes') && thisAction.attr.length == 0) {
-			bootbox.alert('You must select an action to perform with this selector and / or some attributes to grab.');
+			$('#attributes-div').show();
 		} else {
-			actions[thisAction.niceName] = thisAction;
-			var thisActionHtml = 			
-				'<tr>'
-					+ '<td>' + thisAction.niceName + '</td>'
-					+ '<td>' + thisAction.selector + '</td>'
-					+ '<td>' + thisAction.action + '</td> '
-					+ '<td>' + thisAction.attr.join(', ') + '</td>'
-					+ '<td>'
-//						+ '<button type="button" class="btn btn-primary btn-sm"><strong class="glyphicon glyphicon-pencil"></strong></button> '
-						+ '<button type="button" class="btn btn-danger btn-sm"><strong class="glyphicon glyphicon-trash"></strong></button>'
-					+ '</td>'
-				+ '</tr>'
-			;
-
-			$('#actions-list').append(thisActionHtml);
-			$('#actions-list').find('.popover-dismiss').popover();
-
-			$('#add-action-modal').modal('hide');
-			$('#actionscount').html(Object.keys(actions).length);
-			$('#actionslisttab').show().effect('highlight', {}, 2000)
+			$('#attributes-div').hide();
 		}
-	})
-		
-	$('#actions-list').on('click', '.remove', function() {
-		$(this).parents('li').remove();
-	});	
+	}
+
+	$('#add-action-modal').modal();
+});
+
+$('#btn-add-action-modal').click(function(){
+	var thisAction = {
+		niceName: $('#nicename').val().trim()
+		, selector: $('#current-selector').val().trim()
+		, action: ($('#action').val() || 'get attributes')
+		, attr: ($('#attributes').val() || [])
+	};
+
+	if (thisAction.niceName == '') {
+		bootbox.alert('Please give a nice name!');
+	} else if (actions[thisAction.niceName]) {
+		bootbox.alert('This name is already in use !');
+	} else if ((thisAction.action == 'get attributes') && thisAction.attr.length == 0) {
+		bootbox.alert('You must select an action to perform with this selector and / or some attributes to grab.');
+	} else {
+		actions[thisAction.niceName] = thisAction;
+		var thisActionHtml = 			
+		'<tr>'
+		+ '<td>' + thisAction.niceName + '</td>'
+		+ '<td>' + thisAction.selector + '</td>'
+		+ '<td>' + thisAction.action + '</td> '
+		+ '<td>' + thisAction.attr.join(', ') + '</td>'
+		+ '<td>'
+		//						+ '<button type="button" class="btn btn-primary btn-sm"><strong class="glyphicon glyphicon-pencil"></strong></button> '
+		+ '<button type="button" class="btn btn-danger btn-sm"><strong class="glyphicon glyphicon-trash"></strong></button>'
+		+ '</td>'
+		+ '</tr>'
+		;
+
+		$('#actions-list').append(thisActionHtml);
+		$('#actions-list').find('.popover-dismiss').popover();
+
+		$('#add-action-modal').modal('hide');
+		$('#actionscount').html(Object.keys(actions).length);
+		$('#actionslisttab').show().effect('highlight', {}, 2000)
+	}
+});
+
+$('#actions-list').on('click', '.remove', function() {
+	$(this).parents('li').remove();
+});	
 
 
 /*
--- routes/examine.js
+++ routes/examine.js
@@ -1,43 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var scrap = require('scrap');
-var slugify = require('slugify');
+var slugify = require('slug');
 var fs = require('fs');
 
 var tmpDir = __dirname+'/../public/tmp';
 
 var collectGarbage = function() {
-
+  var endTime = new Date().getTime() - 3600000 * 24;
+  
 	fs.readdir(tmpDir, function(err, files) {
 	  files.forEach(function(file, index) {
 	    fs.stat(tmpDir+'/'+file, function(err, stat) {
-	      var endTime, now;
 	      if (err) {
 	        return console.error(err);
 	      }
-	      now = new Date().getTime();
-	      endTime = new Date(stat.ctime).getTime() + 3600000 + 24;
-	      if (now > endTime) {
-	        fs.rmdir(tmpDir+'/'+file, function(){});
+	      var fileTime = new Date(stat.ctime).getTime();
+	      if (fileTime > endTime) {
+	        fs.unlink(tmpDir+'/'+file, function(err){if(err)console.log(err)});
 	      }
 	    });
 	  });
 	});	
 }
 
+/******************************************************************************/
+
 router.post('/', function(req, res) {
 	var url = req.param('url');
+	var tmp =  slugify(url)+'.html';
 	scrap(url, function(err, $, code, html, resp) {
 		if (err) {
 			res.send(500,err);
 		} else {
-			var tmp =  slugify(url)+'.html';
 			try {
+				// try to create the tmp dir in public
+				// ugly thing : fix me
 				fs.mkdirSync(tmpDir);
 			} catch (e) {/* do nothing */}
+
 			fs.unlink(tmpDir+'/'+tmp, function(){
+				// unlink to refresh the ctime
 				fs.writeFile(tmpDir+'/'+tmp, html, function() {
-				  res.render('examine', {host: req.protocol + '://' + req.get('host'), url: url, history: '[]', tmp: tmp});
+					// send the date when the file has been written and available fot http queries
+				  res.render('examine', {
+				  	host: req.protocol + '://' + req.get('host')
+				  	, url: url
+				  	, history: '[]'
+				  	, tmp: tmp
+				  });
 				});
 			});
 		}
@@ -45,6 +56,10 @@ router.post('/', function(req, res) {
 	collectGarbage();
 });
 router.post('/follow', function(req, res) {
-	res.render('examine', {host: req.protocol + '://' + req.get('host'), url: req.param('url'), history: JSON.stringify(req.param('history'))});
+	res.render('examine', {
+		host: req.protocol + '://' + req.get('host')
+		, url: req.param('url')
+		, history: JSON.stringify(req.param('history'))
+	});
 });
 module.exports = router
