d07d24f6b53c0635f4e068ae7d7e150d7a9208a7
Jeff <jeff@modulaweb.fr>
2014-08-11 20:19:59 +0200

    largely refactor scrap process + add full pagination discovery and automatic scrapping

--- routes/scrap.js
+++ routes/scrap.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var iconv = require('iconv-lite');
+var cheerio = require('cheerio');
+var urls = require('url');
 var async = require('async').actions;
 
 var actions = require('./libs/actions');
@@ -11,7 +13,56 @@ var actions = require('./libs/actions');
 router.post('/', function(req, res) {
   var project = JSON.parse(req.param('project')); // load the scrapping project
   project.data = {}; // the data scrapped from url
-  project.pagination = {links:[], selectors:[]}; // the pagination links if any
+  project.pagination = {urls:[project.url], selectors:[], scrapped: 0}; // the pagination links if any
+  																													 // {urls: [array of urls to scrap, the first one is the project url], selectors: [array of links selectors], scrapped: number of pagination urls scrapped}
+	var doScrap = function() {
+		// scrap the pages to scrap
+		console.log('> Scrapping url '+(project.pagination.scrapped +1 )+'/'+project.pagination.urls.length);
+		var url = project.pagination.urls[project.pagination.scrapped]; // use the current url in list
+
+	  request({url: url, encoding: null}, function(err, code, html) {
+	  	html = iconv.decode(html, project.charset); // apply the proper charset to the html content : is it really useful ?
+	  	for (var name in project.actions) {
+	  		// perform all actions and store them into the datastore
+	  		if (actions[project.actions[name].action]) { // ensure that the action really exists
+	  			project.data[name] = actions[project.actions[name].action](html, project.actions[name].selector); // perform the action
+	  		}
+	  	}
+	  	// collects all pagination links hrefs
+	  	var $ = cheerio.load(html);
+	  	for (var i in project.pagination.selectors) {
+	  		$(project.pagination.selectors[i]).each(function() {
+	  			// get the href of all pagination links
+	  			var href = $(this).attr('href');
+	  			if (href) {
+	  				// some time the anchors are just anchor !
+		  			if (href.trim() != '') {
+		  				// sometime hrefs are invalids or relatives : reconstruct an absolute url
+		  				// TODO : take advantage or the base url meta !
+		  				var url = urls.parse(href, true);
+		  				if (!url.hostname) {
+		  					href = urls.resolve(project.url, href);
+		  				}
+		  				if (project.pagination.urls.indexOf(href) == -1) {
+		  					// we can add this url if we did not already have it
+		  					project.pagination.urls.push(href);
+		  				}
+		  			}
+		  		}
+	  		});
+	  	}
+
+	  	project.pagination.scrapped++; // increment the scrapped urls counter
+	  	
+	  	if (project.pagination.urls.length > project.pagination.scrapped) {
+	  		// we do have some more urls to scrap
+	  		doScrap();
+	  	} else {
+	  		console.log('> Done.');
+	  		res.json(project.pagination); // Output the data 'as is' by now for dev
+	  	}
+		});
+	}
 
   // look for pagination links to follow
   for (var name in project.actions) {
@@ -21,17 +72,9 @@ router.post('/', function(req, res) {
   		delete project.actions[name];
   	}
   }
-  // scrap the url page
-  request({url: project.url, encoding: null}, function(err, code, html) {
-  	html = iconv.decode(html, project.charset); // apply the proper charset to the html content : is it really useful ?
-  	for (var name in project.actions) {
-  		// perform all actions and store them into the datastore
-  		if (actions[project.actions[name].action]) { // ensure that the action really exists
-  			project.data[name] = actions[project.actions[name].action](html, project.actions[name].selector); // perform the action
-  		}
-  	}
-  	res.json(project.data); // Output the data 'as is' by now for dev
-  });
+
+  // lest do the scrap things
+  doScrap(); // this function outputs the result
 });
 
 module.exports = router
