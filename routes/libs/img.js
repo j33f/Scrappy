cadaaec3e3cf9e8a044fb2b32f6c3536f5b90699
Jeff <jeff@modulaweb.fr>
2014-09-20 20:14:04 +0200

    debug

--- routes/libs/img.js
+++ routes/libs/img.js
@@ -44,6 +44,7 @@ var getImg = function(img, options) {
 		    path: parsedUrl.path
 		});
 		var response = syncRequest.end();
+		console.dir(response);
 		if (response.statusCode == 200) {
 			result = [{src: 'data:' + response.headers['content-type'] + ';base64,' + response.body.toString('base64')}];
 		} else {
