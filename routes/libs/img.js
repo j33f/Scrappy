3fc70efc9935d907a8362bcdb9773afa51d84cf9
Jeff <jeff@modulaweb.fr>
2014-09-20 22:30:49 +0200

    fix content-type retrieving un img actions

--- routes/libs/img.js
+++ routes/libs/img.js
@@ -44,13 +44,8 @@ var getImg = function(img, options) {
 		    path: parsedUrl.path
 		});
 		var response = syncRequest.end();
-		console.log('________________');
-		console.dir(response);
-		console.log('________________');
 		if (response.statusCode == 200) {
 			var contentType = response.headers['content-type'] || response.headers['Content-Type'];
-			console.log(contentType);
-			console.log('________________');
 			result = [{src: 'data:' + contentType + ';base64,' + response.body.toString('base64')}];
 		} else {
 			result = [{src: response.statusCode + ' Error.'}];
